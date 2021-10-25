f4bf46c666ecce5339430a78165b3a010e228834
adlerzz <adlerzz.true@gmail.com>
2021-10-26 01:58:27 +0300

    add logging refactoring

--- main_flow.py
+++ main_flow.py
@@ -4,8 +4,12 @@ from Mapper import *
 import codec
 import utils
 from WritePointer import *
+import logging
 
 DUPLICATE_SUFFIX = '_DUP_'
+LOG_FORMAT = '%(asctime)s [%(levelname)-5s] %(funcName)s : %(message)s'
+
+logging.basicConfig(filename='app.log', level=logging.DEBUG, format=LOG_FORMAT)
 
 
 class Reader:
@@ -14,10 +18,9 @@ class Reader:
 
     def handle_file_header(self):
         signature = self.f.read_bytes(4)
-        print(signature)
         if signature != b'8BPS':
             raise Exception("Bad format")
-        version = f.read_int(2)
+        version = self.f.read_int(2)
         if version == 1:
             pass
         elif version == 2:
@@ -48,115 +51,135 @@ class Reader:
         return extra_fields_len, d
 
 
-# f = Mapper("orthodox_xmas_angel_test.psd")
-f = Mapper("test.psd")
+class ActionComposer:
 
-r = Reader(f)
-r.handle_file_header()
-r.handle_color_mode_data()
-r.handle_image_resources()
+    def __init__(self):
+        self.__f = None
+        self.__lm_data_length = None
+        self.__l_data_length = None
+        self.__layer_names = dict()
+        self.__layer_pointers = list()
+        self.__layer_wpointers = list()
+        self.__lmd = None
+        self.__ld = None
 
-lm_data_length = f.mark_uint32()
-print(f'lm_data_length: {lm_data_length}')
-l_data_length = f.mark_uint32()
-print(f'l_data_length: {l_data_length}')
+    def do_read(self, filename):
+        logging.info(f'start reading with "{filename}"')
+        self.__f = Mapper(filename)
+        r = Reader(self.__f)
+        r.handle_file_header()
+        r.handle_color_mode_data()
+        r.handle_image_resources()
 
-layer_count = abs(f.read_int(2, True))
-print(f'layer_count: {layer_count}')
+        self.__lm_data_length = self.__f.mark_uint32()
+        logging.debug(f'lm_data_length: {self.__lm_data_length}')
 
-layer_pointers = list()
-layer_names = dict()
+        self.__l_data_length = self.__f.mark_uint32()
+        logging.debug(f'l_data_length: {self.__l_data_length}')
 
-print('----')
-for i in range(0, layer_count):
+        layer_count = abs(self.__f.read_int(2, True))
+        logging.debug(f'layer_count: {layer_count}')
 
-    # print('###{} layer###'.format(i+1))
-    (extra_fields_length, delta) = r.handle_layer_pre()
-    layer_name = f.mark_str()
-    sh = extra_fields_length.value - delta - layer_name.size
+        for i in range(0, layer_count):
 
-    while sh > 8:
-        a_signature = f.read_bytes(4)
-        if a_signature != b'8BIM' and a_signature != b'8B64':
-            raise Exception("File is corrupted")
-        a_key = f.read_bytes(4)
-        a_value = f.mark_utf16()
-        if a_key == b'luni':
-            u_layer_name = a_value.value[4:].decode(codec.UNICODE).rstrip('\x00')
+            # print('###{} layer###'.format(i+1))
+            (extra_fields_length, delta) = r.handle_layer_pre()
+            layer_name = self.__f.mark_str()
+            sh = extra_fields_length.value - delta - layer_name.size
 
-            new_layer_name = codec.translate_name(u_layer_name)
+            while sh > 8:
+                a_signature = self.__f.read_bytes(4)
+                if a_signature != b'8BIM' and a_signature != b'8B64':
+                    raise Exception("File is corrupted")
+                a_key = self.__f.read_bytes(4)
+                a_value = self.__f.mark_utf16()
+                if a_key == b'luni':
+                    u_layer_name = a_value.value[4:].decode(codec.UNICODE).rstrip('\x00')
 
-            if not new_layer_name.startswith('</') and new_layer_name != layer_name.value:
-                utils.inc(layer_names, new_layer_name)
+                    new_layer_name = codec.translate_name(u_layer_name)
 
-                layer_pointers.append((extra_fields_length, layer_name, a_value))
+                    if not new_layer_name.startswith('</') and new_layer_name != layer_name.value:
+                        utils.inc(self.__layer_names, new_layer_name)
 
-        sh = sh - 8 - a_value.size
+                        self.__layer_pointers.append((extra_fields_length, layer_name, a_value))
 
-    if sh > 0:
-        f.move_on(sh)
+                sh = sh - 8 - a_value.size
 
-# f.save("orthodox_xmas_angel_test_ch.psd")
-# f.save("golden_wheel_ch.psd")
+        if sh > 0:
+            self.__f.move_on(sh)
 
-print(layer_names)
-layer_wpointers = list()
+        logging.debug(f'layer_names: {self.__layer_names}')
 
-for p in layer_pointers:
-    (ex, layer, u_layer) = p
-    # print(layer, u_layer)
-    new_layer_name = codec.translate_name(layer.value)
-    p = layer_names[new_layer_name]
-    layer_names[new_layer_name] = p - 1
-    if p > 1:
-        new_layer_name = new_layer_name + DUPLICATE_SUFFIX + str(p)
+    def do_translate(self):
 
-    ln = WritePointer(layer, new_layer_name, utils.get_padded_length(new_layer_name))
+        for p in self.__layer_pointers:
+            (ex, layer, u_layer) = p
+            # print(layer, u_layer)
+            new_layer_name = codec.translate_name(layer.value)
+            p = self.__layer_names[new_layer_name]
+            self.__layer_names[new_layer_name] = p - 1
+            if p > 1:
+                new_layer_name = new_layer_name + DUPLICATE_SUFFIX + str(p)
 
-    u_b = len(new_layer_name).to_bytes(4, 'big') + bytes(new_layer_name, codec.UNICODE) + b'\x00\x00'
-    u_l = len(u_b).to_bytes(4, 'big')
-    un = WritePointer(u_layer, u_l + u_b, len(u_b) + 4)
+            ln = WritePointer(layer, new_layer_name, utils.get_padded_length(new_layer_name))
 
-    n_ex = WritePointer(ex, ex.value + ln.offset() + un.offset(), ex.size)
+            u_b = len(new_layer_name).to_bytes(4, 'big') + bytes(new_layer_name, codec.UNICODE) + b'\x00\x00'
+            u_l = len(u_b).to_bytes(4, 'big')
+            un = WritePointer(u_layer, u_l + u_b, len(u_b) + 4)
 
-    layer_wpointers.append((n_ex, ln, un))
+            n_ex = WritePointer(ex, ex.value + ln.offset() + un.offset(), ex.size)
 
-sh = 0
+            self.__layer_wpointers.append((n_ex, ln, un))
 
-for p in layer_wpointers:
-    (ex, ln, un) = p
-    ex.shift(sh)
-    sh = sh + ex.offset()
-    # print(ex)
+    def do_shifts(self):
+        sh = 0
 
-    ln.shift(sh)
-    sh = sh + ln.offset()
-    # print(ln)
+        for p in self.__layer_wpointers:
+            (ex, ln, un) = p
+            ex.shift(sh)
+            sh = sh + ex.offset()
+            # print(ex)
 
-    un.shift(sh)
-    sh = sh + un.offset()
-    # print(un)
+            ln.shift(sh)
+            sh = sh + ln.offset()
+            # print(ln)
 
-print(f'Summary size shift : {sh}')
+            un.shift(sh)
+            sh = sh + un.offset()
+            # print(un)
 
-lmd = WritePointer(lm_data_length, lm_data_length.value + sh, lm_data_length.size)
-ld = WritePointer(l_data_length, l_data_length.value + sh, l_data_length.size)
+        logging.debug(f'Summary size shift : {sh}')
 
-f.write_pointer(lmd)
-f.write_pointer(ld)
+        self.__lmd = WritePointer(self.__lm_data_length, self.__lm_data_length.value + sh, self.__lm_data_length.size)
+        self.__ld = WritePointer(self.__l_data_length, self.__l_data_length.value + sh, self.__l_data_length.size)
 
-l = 1
-for p in layer_wpointers:
-    (ex, ln, un) = p
-    f.write_pointer(ex)
-    f.write_pointer(ln)
-    f.write_pointer(un)
+    def do_write(self, filename):
+        self.__f.write_pointer(self.__lmd)
+        self.__f.write_pointer(self.__ld)
+
+        counter = 1
+        for p in self.__layer_wpointers:
+            (ex, ln, un) = p
+            self.__f.write_pointer(ex)
+            self.__f.write_pointer(ln)
+            self.__f.write_pointer(un)
+            logging.debug(ex)
+            logging.debug(ln)
+            logging.debug(un)
+            logging.debug('{} of {}'.format(counter, len(self.__layer_wpointers)))
+            print('{} of {}'.format(counter, len(self.__layer_wpointers)))
+            counter = counter + 1
+
+        self.__f.save(filename)
+        logging.info(f'stored to"{filename}"')
+
+
+try:
+    ac = ActionComposer()
+    ac.do_read('test.psd')
+    ac.do_translate()
+    ac.do_shifts()
+    ac.do_write('handled.psd')
+except Exception as ex:
+    logging.error(ex)
     print(ex)
-    print(ln)
-    print(un)
-    print('{} of {}'.format(l, len(layer_wpointers)))
-    l = l+1
-
-f.save('test_handled.psd')
-
-
